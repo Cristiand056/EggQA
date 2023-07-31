30ab7e6caccc684abc0e77927b9bee4f6e464436
Cristian <david7891235@gmail.com>
2023-07-31 18:27:00 -0500

    Java Collections Encuentro 8

---
+++ Java/JavaCollections/JavaCollectionsEjerciciosExtra/JavaCollectionsEjerExtra4/src/javacollectionsejerextra4/data/CodigosPostales.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javacollectionsejerextra4.data;
+
+/**
+ *
+ * @author USER
+ */
+public class CodigosPostales {
+
+    public static final String[] codigos =
+    {
+        "110141", "052850", "154201", "730017", "662001", "410001", "274010", "526001", "250240", "081020"
+    };
+    public static final String[] ciudades =
+    {
+        "bogota", "anori", "arcabuco", "altamira", "azufral", "acropolis", "istmina", "ancuya", "chuntame", "tubara"
+    };
+}
